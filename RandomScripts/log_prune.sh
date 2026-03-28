ba53b50e7ad40f1941973e06ebacdbd1d41ca2d9
wemetinperson <121180206+wemetinperson@users.noreply.github.com>
2026-03-28 14:16:02 +0000

    Update log_prune.sh

--- RandomScripts/log_prune.sh
+++ RandomScripts/log_prune.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # www.theutsguy.com
 
-#This will check the files (typically log files) in the "directory_name" directory and will delete the oldest logs if the disk is more than "max_space percent" full
-#I use this to manage the logs in the FiPi thing I described in this video: https://youtu.be/k2mtcIc_QAg
+# This will check the files (typically log files) in the "directory_name" directory and will delete the oldest logs if the disk is more than "max_space percent" full
+# I use this to manage the logs in the FiPi thing I described in this video: https://youtu.be/k2mtcIc_QAg
+# I recommend having this run as a service or in your crontab at regular intervals 
 
 # This is a random thing I made, don't use it for anything.
 # Don't listen to anything I say or do anything I do.
@@ -10,7 +11,7 @@
 #This looks for the largest disk in the system and assumes that's the same disk as the logs/directory you are trying to prune.
 percent=$(df -h | sort -k3 -hr | head -n1 | awk '{gsub("%","",$5); print $5}')
 
-#Modify these two variables as you see fit.
+#Modify these two variables as you see fit, it's the percent of disk space that's acceptable to have full and the directory you want to prune.
 max_space=90
 directory_name="/home/theutsguy/uts_kismet_logs"
 
